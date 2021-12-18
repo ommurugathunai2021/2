b67a510989d677ba06df27754396e2388986f7db
ommurugathunai2021 <83066571+ommurugathunai2021@users.noreply.github.com>
2021-12-18 22:25:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import './App.css';
+
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+
+function App() {
+
+  return (
+    
+
+    <div className="container">
+
+      <Header/>
+      <Footer/>
+      <Home/>
+
+      </div>
+  );
+}
+
+  
+export default App;
---
+++ products_data.js
@@ -0,0 +1,58 @@
+const product_card=[
+    {
+        id:1,
+        product_name:"Car1",
+        description: "benz",
+        price:100,
+        currency:"$",
+        thumb:'./images/1.png'
+
+    
+
+    },
+    {
+        id:2,
+        product_name:"Car2",
+        description: "xxxxxxxxxx",
+        price:100,
+        currency:"$",
+        thumb:"./images/2.png"
+
+    
+
+    },
+    {
+        id:3,
+        product_name:"Car3",
+        description: "xxxxxxxxxx",
+        price:100,
+        currency:"$",
+        thumb:".images/3.png"
+
+    
+
+    },
+    {
+        id:4,
+        product_name:"Car4",
+        description: "xxxxxxxxxx",
+        price:100,
+        currency:"$",
+        thumb:"./images/1.png"
+
+    
+
+    },
+    {
+        id:5,
+        product_name:"Car5",
+        description: "xxxxxxxxxx",
+        price:100,
+        currency:"$",
+       URL:"/images/4.png"
+
+    
+
+    },
+]
+export default product_card;
